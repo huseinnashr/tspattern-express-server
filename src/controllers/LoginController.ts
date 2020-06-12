ae3a56d2704f7469dc5ab92c371d59ee1e7e32f0
Husein <nvdhunter007@gmail.com>
2020-06-12 22:05:44 +0700

    rewrite login post route to a decorator pattern

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { controller, get } from "./decorators";
+import { controller, get, bodyValidator, post } from "./decorators";
 
 @controller("/auth")
 export class LoginController {
@@ -18,5 +18,19 @@ export class LoginController {
         <button>Submit</button>
       </form>
     `);
+  }
+
+  @post("/login")
+  @bodyValidator("email", "password")
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email && password && email === "hi@hi.com" && password === "password") {
+      req.session!.loggedIn = true;
+      console.log(req.session);
+      res.redirect("/");
+    } else {
+      res.send("Invalid email or password");
+    }
   }
 }
